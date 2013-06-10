a6872370e27be2a041f316ccec2ab130fe1d9f47
Christoph Dorn <christoph@christophdorn.com>
2013-06-10 10:48:36 -0700

    set WEBRTC_SHIM browser global if included via script tag

--- webrtc-shim.js
+++ webrtc-shim.js
@@ -1,10 +1,10 @@
-(function (root, factory) {
+(function (globals, factory) {
     if (typeof define === 'function' && define.amd) {
       // AMD. Register as an anonymous module.
       define(factory);
     } else {
-      // Browser globals
-      root.amdWeb = factory();
+      // Browser global
+      globals.WEBRTC_SHIM = factory();
     }
 }(this, function () {
 
